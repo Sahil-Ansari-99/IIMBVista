3d0732bab080ac56e8e6bd8840c7dff1fee6329c
Sahil-Ansari-99 <39992186+Sahil-Ansari-99@users.noreply.github.com>
2019-04-01 02:23:41 +0530

    home page

--- app/src/main/java/com/iimbvista/iimbvista/MainActivity.java
+++ app/src/main/java/com/iimbvista/iimbvista/MainActivity.java
@@ -1,13 +1,32 @@
 package com.iimbvista.iimbvista;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.home_drawer_layout);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
