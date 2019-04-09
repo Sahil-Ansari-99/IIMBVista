1840fb558506b28d95a2a931f64af6aed26e235f
neilghosh2000 <39198528+neilghosh2000@users.noreply.github.com>
2019-04-10 00:26:08 +0530

    Corrected php files for sponsors page

--- app/src/main/java/com/iimbvista/iimbvista/TestActivity.java
+++ app/src/main/java/com/iimbvista/iimbvista/TestActivity.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -14,6 +16,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.iimbvista.iimbvista.Sponsors.SponsorsActivity;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
 
@@ -22,6 +25,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.StringTokenizer;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -29,16 +33,19 @@ import okhttp3.Callback;
 
 public class TestActivity extends AppCompatActivity {
 
-    TextView textView;
-    public static String API_LINK="http://www.iimb-vista.com/2019/app_api/get_nonce/?controller=user&method=register";
+    ImageView imageView;
+    String responseString[];
+    public static String API_LINK="http://www.iimb-vista.com/2019/vista_app_sponsors.php?id=1";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dummy_sponsor);
 
-        textView=(TextView)findViewById(R.id.dummy_tv);
+//        imageView=(ImageView)findViewById(R.id.dummy_tv);
         getData();
+
+//        Picasso.with(getApplicationContext()).load(API_LINK).into(imageView);
     }
 
     public void getData(){
@@ -48,9 +55,16 @@ public class TestActivity extends AppCompatActivity {
             @Override
             public void onResponse(String response) {
                 try{
-                    JSONObject jObj = new JSONObject(response);
-                    String status = jObj.getString("nonce");
-                    textView.setText(status);
+//                    JSONObject jObj = new JSONObject(response);
+//                    String status = jObj.getString("nonce");
+//                    textView.setText("success");
+
+                    StringTokenizer tokenizer=new StringTokenizer(response," ");
+                    String name=tokenizer.nextToken();
+//                    responseString=response.split(" ");
+                    Log.e("test",name);
+//                    Picasso.with(getApplicationContext()).load(response).into(imageView);
+                    Log.e("stringRequest",response);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
@@ -58,7 +72,7 @@ public class TestActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText(error.toString());
+//                textView.setText(error.toString());
             }
         });
 
