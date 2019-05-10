2a7d155205c8eade2e423c3361fa343fd469be7f
neilghosh2000 <39198528+neilghosh2000@users.noreply.github.com>
2019-05-10 17:02:24 +0530

    Added Login and Profile Activity

---
+++ app/src/main/java/com/iimbvista/iimbvista/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.iimbvista.iimbvista;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email=findViewById(R.id.login_email);
+        password=findViewById(R.id.login_password);
+        btn_login=findViewById(R.id.login_button);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+            }
+        });
+    }
+}
